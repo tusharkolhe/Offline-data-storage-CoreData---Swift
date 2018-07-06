07921627640d152bc4fc083ca68e97902c8b5af6
“Tushar <tushar.kolhe01@gmail.com>
2018-07-06 19:37:13 +0530

    Save data

--- TKCoreData/ViewController.swift
+++ TKCoreData/ViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+
+    @IBOutlet var nameTF: UITextField!
+    @IBOutlet var profileTF: UITextField!
+    var employee: [NSManagedObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +26,35 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func saveButton(_ sender: Any) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Employee",
+                                                in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        person.setValue(nameTF.text, forKeyPath: "name")
+        person.setValue(profileTF.text, forKeyPath: "profile")
+
+        do {
+            try managedContext.save()
+            employee.append(person)
+            let alertController = UIAlertController(title: "Hey", message: "Data saved successfully!!", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
 }
 
